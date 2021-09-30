b97b87796b985b434156a12f32ceb3fed2371633
aguestuser <aguestuser@riseup.net>
2021-09-30 00:35:02 -0400

    [0] make k/v store async using channels

---
+++ src/error.rs
@@ -0,0 +1,20 @@
+// #[cfg(feature = "std")]
+// use std::error::Error;
+// #[cfg(not(feature = "std"))]
+// use std::fmt::Display;
+//
+// use err_derive::Error;
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+// #[derive(Debug, Error)]
+// pub enum FormatError {
+//     #[error(display = "invalid header (expected: {:?}, got: {:?})", expected, found)]
+//     InvalidHeader {
+//         expected: String,
+//         found: String,
+//     },
+//     #[error(display = "missing attribute: {:?}", _0)]
+//     MissingAttribute(String),
+// }
+//
--- src/store.rs
+++ src/store.rs
@@ -1,34 +1,67 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
 
-// pub type Error = Box<dyn std::error::Error + Send + Sync>;
-pub type Error = Box<dyn std::error::Error>;
-// pub type Result<T> = std::result::Result<T, Error>;
+use tokio::sync::{mpsc, oneshot};
+use tokio::sync::mpsc::Sender;
 
-pub type Db = Arc<Mutex<HashMap<String, String>>>;
+use crate::error::Result;
+use crate::store::StoreCommand::{Get, Set};
 
+pub type Db = HashMap<String, String>;
+pub type Responder<T> = oneshot::Sender<Result<T>>;
+
+#[derive(Debug)]
+pub enum StoreCommand {
+    Get {
+        key: String,
+        resp: Responder<Option<String>>,
+    },
+    Set {
+        key: String,
+        val: String,
+        resp: Responder<()>,
+    }
+}
 
 pub struct Store {
-    db: Db
+    tx: Sender<StoreCommand>,
 }
 
 impl Store {
-    fn new() -> Store {
-        Self {
-            db: Arc::new(Mutex::new(HashMap::new())),
-        }
+
+    async fn run() -> Store {
+        let mut db: Db = HashMap::new();
+        let (tx, mut rx) = mpsc::channel::<StoreCommand>(1);
+
+        // use an actor to enforce thread-safe writes/reads on the store's backing hashmap
+        tokio::spawn(async move {
+            while let Some(cmd) = rx.recv().await {
+                match cmd {
+                    Get { key, resp } => {
+                        let val = db.get(&key).map(|s| s.clone());
+                        let _ = resp.send(Ok(val));
+                    },
+                    Set { key, val, resp } => {
+                        db.insert(key, val);
+                        let _ = resp.send(Ok(()));
+                    }
+                }
+            }
+        });
+
+        Store { tx }
     }
 
-    fn set(&mut self, key: &str, val: &str) -> Result<(), Error> {
-        let mut db = self.db.lock().expect("Failed to acquire lock.");
-        db.insert(key.to_string(), val.to_string()).ok_or("Write failed")?;
-        Ok(())
 
+    async fn set(&mut self, key: String, val: String) -> Result<()> {
+        let (res_tx, res_rx) = oneshot::channel();
+        self.tx.send(StoreCommand::Set{ key, val, resp: res_tx }).await?;
+        res_rx.await?
     }
 
-    fn get(&mut self, key: &str) -> Result<Option<String>, Error> {
-        let db = self.db.lock().expect("Failed to acquire lock.");
-        Ok(db.get(key).map(|s| s.clone()))
+    async fn get(&mut self, key: String) -> Result<Option<String>> {
+        let (res_tx, res_rx) = oneshot::channel();
+        self.tx.send(StoreCommand::Get{ key, resp: res_tx }).await?;
+        res_rx.await?
     }
 
 }
@@ -37,12 +70,12 @@ impl Store {
 mod store_tests {
     use super::*;
 
-    #[test]
-    fn set_and_get() {
-        let mut store = Store::new();
-        store.set("foo", "bar").unwrap();
+    #[tokio::test(flavor ="multi_thread")]
+    async fn set_and_get() {
+        let mut store = Store::run().await;
+        store.set("foo".to_string(), "bar".to_string()).await.unwrap();
         assert_eq!(
-            store.get("foo").unwrap(),
+            store.get("foo".to_string()).await.unwrap(),
             Some("bar".to_string()),
         )
     }
