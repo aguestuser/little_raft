806715e3ab315d3c7977bffba0730cb4129780e9
aguestuser <aguestuser@riseup.net>
2021-10-19 19:41:09 -0400

    [1] client can write to a peer server

--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,9 @@
-use crate::error::Result;
+use crate::error::{IllegalStateError, Result};
 use crate::node::State;
 use crate::tcp::connection::Connection;
 use dashmap::DashMap;
-use futures::stream::FuturesUnordered;
 use futures::StreamExt;
-use std::hash::Hash;
 use std::net::SocketAddr;
-use std::sync::Arc;
 use tokio::net::TcpSocket;
 
 pub struct Client {
@@ -44,6 +41,16 @@ impl Client {
         });
         Ok(())
     }
+
+    async fn write(&mut self, server_addr: &str, msg: Vec<u8>) -> Result<()> {
+        if let Some(mut conn) = self.connections.get_mut(server_addr) {
+            conn.write(msg).await
+        } else {
+            Err(Box::new(IllegalStateError::NoPeerAtAddress(
+                server_addr.to_string(),
+            )))
+        }
+    }
 }
 
 #[cfg(test)]
@@ -54,49 +61,105 @@ mod test_client {
     use tokio::sync::mpsc;
     use tokio::sync::mpsc::Receiver;
 
-    lazy_static! {
-        static ref addr: SocketAddr = Gen::socket_addr();
-        static ref server_addrs: Vec<SocketAddr> =
-            vec![Gen::socket_addr(), Gen::socket_addr(), Gen::socket_addr()];
+    struct Runner {
+        client_addr: SocketAddr,
+        server_addrs: Vec<SocketAddr>,
+        addr_rx: Receiver<SocketAddr>,
+        msg_rx: Receiver<(SocketAddr, Vec<u8>)>,
     }
 
     #[feature(async_closure)]
-    async fn setup() -> Receiver<SocketAddr> {
-        let (addr_tx, addr_rx) = mpsc::channel::<SocketAddr>(3);
-        for i in 0..server_addrs.len() {
-            let listener = TcpListener::bind(server_addrs[i]).await.unwrap();
-            let tx = addr_tx.clone();
+    async fn setup() -> Runner {
+        let buf_size = 10;
+        let client_addr: SocketAddr = Gen::socket_addr();
+        let server_addrs: Vec<SocketAddr> =
+            vec![Gen::socket_addr(), Gen::socket_addr(), Gen::socket_addr()];
+
+        let (addr_tx, addr_rx) = mpsc::channel::<SocketAddr>(buf_size);
+        let (msg_tx, msg_rx) = mpsc::channel::<(SocketAddr, Vec<u8>)>(buf_size);
+
+        for server_addr in server_addrs.clone().into_iter() {
+            let listener = TcpListener::bind(server_addr).await.unwrap();
+            let addr_tx = addr_tx.clone();
+            let msg_tx = msg_tx.clone();
+
             tokio::spawn(async move {
                 loop {
                     let (socket, client_addr) = listener.accept().await.unwrap();
-                    tx.send(client_addr.clone()).await.unwrap();
+                    addr_tx.send(client_addr.clone()).await.unwrap();
+                    let msg_tx = msg_tx.clone();
+
+                    tokio::spawn(async move {
+                        let mut conn = Connection::new(socket);
+                        loop {
+                            let msg = conn.read().await.unwrap();
+                            msg_tx.send((server_addr, msg)).await.unwrap();
+                        }
+                    });
                 }
             });
         }
-        return addr_rx;
+
+        return Runner {
+            client_addr,
+            server_addrs,
+            addr_rx,
+            msg_rx,
+        };
     }
 
     #[tokio::test]
     async fn constructs_itself() {
-        let client = Client::new(*addr, server_addrs.clone());
+        let client_addr = Gen::socket_addr();
+        let server_addrs = vec![Gen::socket_addr(), Gen::socket_addr(), Gen::socket_addr()];
+        let client = Client::new(client_addr, server_addrs.clone());
 
-        assert_eq!(client.address, *addr);
+        assert_eq!(client.address, client_addr);
         assert_eq!(client.server_addresses, server_addrs.clone());
         assert!(client.connections.is_empty());
     }
 
     #[tokio::test]
     async fn connects_to_servers() {
-        let mut connected_addr_receiver = setup().await;
+        let Runner {
+            client_addr,
+            server_addrs,
+            mut addr_rx,
+            ..
+        } = setup().await;
         let mut connected_addrs = Vec::<SocketAddr>::new();
 
-        let mut client = Client::new(*addr, server_addrs.clone());
+        let mut client = Client::new(client_addr, server_addrs.clone());
         client.run().await.unwrap();
         for _ in 0..server_addrs.len() {
-            connected_addrs.push(connected_addr_receiver.recv().await.unwrap());
+            connected_addrs.push(addr_rx.recv().await.unwrap());
         }
 
         assert_eq!(connected_addrs.len(), server_addrs.len());
         assert_eq!(client.connections.len(), server_addrs.len())
+    }
+
+    #[tokio::test]
+    async fn writes_to_a_server() {
+        let Runner {
+            client_addr,
+            server_addrs,
+            mut addr_rx,
+            mut msg_rx,
+            ..
+        } = setup().await;
+
+        let mut client = Client::new(client_addr, server_addrs.clone());
+        client.run().await.unwrap();
+        for _ in 0..2 {
+            let _ = addr_rx.recv().await.unwrap();
+        }
+
+        let _ = client
+            .write(&server_addrs[0].to_string(), b"hello".to_vec())
+            .await;
+        let (a, msg) = msg_rx.recv().await.unwrap();
+
+        assert_eq!(msg, b"hello\n".to_vec());
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,19 @@
-// #[cfg(feature = "std")]
-// use std::error::Error;
-// #[cfg(not(feature = "std"))]
-// use std::fmt::Display;
-//
-// use err_derive::Error;
+#[cfg(feature = "std")]
+use std::error::Error;
+#[cfg(not(feature = "std"))]
+use std::fmt::Display;
+
+use err_derive::Error;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
+#[derive(Debug, Error)]
+pub enum IllegalStateError {
+    #[error(display = "no record of peer at address: {:?}", _0)]
+    NoPeerAtAddress(String),
+}
+
+//
 // #[derive(Debug, Error)]
 // pub enum FormatError {
 //     #[error(display = "invalid header (expected: {:?}, got: {:?})", expected, found)]
